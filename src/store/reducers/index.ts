e4b81cdcf6673cee62ba39afbba6d880c39be5be
HennessyZazzz <60851577+HennessyZazzz@users.noreply.github.com>
2021-12-10 17:35:33 +0200

    show list and paginate

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import { authReducer } from "../../components/auth/Login/reducer";
 import { registerReducer } from "../../components/auth/Register/reducer";
+import { ProductsReducer } from "../../components/products/reducer";
 
 export const rootReducer = combineReducers({
     auth: authReducer,
     register: registerReducer,
+    product: ProductsReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
